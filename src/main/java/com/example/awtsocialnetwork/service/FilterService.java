1c395e56477c932295ea415c9d3f1e95d0174fc2
sabiralizada <sabir.alizadeh@gmail.vom>
2017-07-11 11:20:33 +0200

    Table structure for Tweet and TwitterUser tables were modified.

---
+++ src/main/java/com/example/awtsocialnetwork/service/FilterService.java
@@ -0,0 +1,16 @@
+package com.example.awtsocialnetwork.service;
+
+import com.example.awtsocialnetwork.repository.FilterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sabiralizada on 7/10/17.
+ */
+@Service
+public class FilterService {
+    @Autowired
+    private FilterRepository filterRepository;
+
+
+}
